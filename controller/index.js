0f11e62031fc10f8522813daa4d1fcced8b8a64b
GuYue <guyue.jiwei@gmail.com>
2016-03-11 22:43:51 +0800

    add icons页面

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+
+function getIcons() {
+    return function (done) {
+        fs.readdir('static/sass/icons', done);
+    };
+}
+
 module.exports = {
     index: function *() {
         this.state.title = 'REBASE';
         yield this.render('index', {
             csrf: this.csrf,
+        });
+    },
+
+    icons: function *() {
+        this.state.title = 'REBASE';
+
+        let icons = yield getIcons();
+        icons = icons.filter((icon) => {
+            if (!/^\w+(?:-\w+)*\.(?:svg)$/i.test(icon)) {
+                console.log('错误图片：', icon);
+                return false;
+            }
+            return true;
+        });
+
+        yield this.render('icons', {
+            csrf: this.csrf,
+            icons: icons,
         });
     },
 };
--- router/index.js
+++ router/index.js
@@ -3,6 +3,7 @@ const controller = require('../controller/index');
 
 
 router.get('/', controller.index);
+router.get('icons', controller.icons);
 
 
 module.exports = router;
