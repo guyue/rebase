d68b6cd04d5ed97810e6533591d67003d6b22c90
GuYue <guyue.jiwei@gmail.com>
2016-05-05 16:09:09 +0800

    添加Modal Event测试用例

--- static/plugin/modal.spec.js
+++ static/plugin/modal.spec.js
@@ -25,6 +25,54 @@ describe('Modal Test Suite', function () {
         }
     });
 
+    var modalElement;
+
+    beforeEach(function () {
+        var div = document.createElement('div');
+        div.innerHTML = '<div class="modal">' +
+                '<header class="bar bar-nav">' +
+                    '<button class="icon icon-cross pull-right" data-toggle="modal-close"></button>' +
+                    '<h1 class="title">红莓花儿开</h1>' +
+                '</header>' +
+                '<div class="content">' +
+                    '<div class="content-padded">' +
+                        '<p>田野小河边，红莓花儿开，有一位少年真使我心爱，可是我不能对他表白，满怀的心腹话儿没法讲出来！</p>' +
+                    '</div>' +
+                '</div>' +
+            '</div>';
+        modalElement = div.firstElementChild;
+
+    });
+
+    afterEach(function () {
+        if (modalElement.parentNode) {
+            modalElement.parentNode.removeChild(modalElement);
+        }
+        modalElement = null;
+    });
+
+    function isOpen() {
+        var style = window.getComputedStyle(modalElement);
+        return modalElement.offsetTop === 0 &&
+            modalElement.offsetLeft === 0 &&
+            modalElement.offsetWidth === window.innerWidth &&
+            modalElement.offsetHeight === window.innerHeight &&
+            parseInt(style.opacity, 10) === 1;
+    }
+
+    function asyncExpect(callback) {
+        var ratchet = document.querySelector('link[href$="/sass/ratchet.css"]');
+        var transitionDuration;
+        [].slice.call(ratchet.sheet.cssRules).forEach(function (rule) {
+            if (rule.selectorText === '.modal') {
+                // 此处不考虑单位，默认单位为s
+                transitionDuration = parseFloat(rule.style.transitionDuration || rule.style.webkitTransitionDuration) * 1000 || 0;
+                // 并默认 + 100s
+                transitionDuration += 100;
+            }
+        });
+        setTimeout(callback, transitionDuration);
+    }
 
     describe('Constructor', function () {
 
@@ -53,54 +101,6 @@ describe('Modal Test Suite', function () {
     });
 
     describe('Modal.prototype', function () {
-        var modalElement;
-
-        beforeEach(function () {
-            var div = document.createElement('div');
-            div.innerHTML = '<div class="modal">' +
-                    '<header class="bar bar-nav">' +
-                        '<button class="icon icon-cross pull-right" data-toggle="modal-close"></button>' +
-                        '<h1 class="title">红莓花儿开</h1>' +
-                    '</header>' +
-                    '<div class="content">' +
-                        '<div class="content-padded">' +
-                            '<p>田野小河边，红莓花儿开，有一位少年真使我心爱，可是我不能对他表白，满怀的心腹话儿没法讲出来！</p>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>';
-            modalElement = div.firstElementChild;
-
-        });
-
-        afterEach(function () {
-            if (modalElement.parentNode) {
-                modalElement.parentNode.removeChild(modalElement);
-            }
-            modalElement = null;
-        });
-
-        function isOpen() {
-            var style = window.getComputedStyle(modalElement);
-            return modalElement.offsetTop === 0 &&
-                modalElement.offsetLeft === 0 &&
-                modalElement.offsetWidth === window.innerWidth &&
-                modalElement.offsetHeight === window.innerHeight &&
-                parseInt(style.opacity, 10) === 1;
-        }
-
-        function asyncExpect(callback) {
-            var ratchet = document.querySelector('link[href$="/sass/ratchet.css"]');
-            var transitionDuration;
-            [].slice.call(ratchet.sheet.cssRules).forEach(function (rule) {
-                if (rule.selectorText === '.modal') {
-                    // 此处不考虑单位，默认单位为s
-                    transitionDuration = parseFloat(rule.style.transitionDuration || rule.style.webkitTransitionDuration) * 1000 || 0;
-                    // 并默认 + 100s
-                    transitionDuration += 100;
-                }
-            });
-            setTimeout(callback, transitionDuration);
-        }
 
         describe('open', function () {
  
@@ -154,6 +154,77 @@ describe('Modal Test Suite', function () {
  
         });
 
+    });
+
+    describe('Event', function () {
+        describe('open', function () {
+            it('open方法执行后触发open事件', inject(function (Modal, done) {
+                var modal = new Modal(modalElement);
+                modalElement.addEventListener('open', function (e) {
+                    done();
+                }, false);
+                modal.open();
+            }, true));
+
+            it('阻止open事件，Modal不会打开', inject(function (Modal, done) {
+                var modal = new Modal(modalElement);
+                modalElement.addEventListener('open', function (e) {
+                    e.preventDefault();
+                    expect(isOpen()).to.be.false;
+                    done();
+                }, false);
+                modal.open();
+            }, true));
+        });
+
+        describe('opened', function () {
+            it('Modal窗口打开后会触发opened事件', inject(function (Modal, done) {
+                var modal = new Modal(modalElement);
+                modalElement.addEventListener('opened', function (e) {
+                    done();
+                }, false);
+                modal.open();
+            }, true));
+        });
+
+        describe('close', function () {
+            it('close方法执行后触发close事件', inject(function (Modal, done) {
+                var modal = new Modal(modalElement);
+                modalElement.addEventListener('close', function (e) {
+                    done();
+                }, false);
+                modal.open();
+                asyncExpect(function () {
+                    modal.close();
+                });
+            }, true));
+
+            it('阻止close事件，Modal不会关闭', inject(function (Modal, done) {
+                var modal = new Modal(modalElement);
+                modalElement.addEventListener('close', function (e) {
+                    e.preventDefault();
+                    expect(isOpen()).to.be.true;
+                    done();
+                }, false);
+                modal.open();
+                asyncExpect(function () {
+                    modal.close();
+                });
+            }, true));
+        });
+
+        describe('closed', function () {
+            it('Modal窗口关闭后会触发closed事件', inject(function (Modal, done) {
+                var modal = new Modal(modalElement);
+                modalElement.addEventListener('closed', function (e) {
+                    done();
+                }, false);
+                modal.open();
+                asyncExpect(function () {
+                    modal.close();
+                });
+            }, true));
+        });
     });
 
 });
