a16fd21ed2ecd226c5eafd7333e8aa18c5a57176
Hu Jiwei <hu_jiwei@foxmail.com>
2016-11-03 20:23:45 +0800

    页面上添加结果展示

--- static/js/sliders.js
+++ static/js/sliders.js
@@ -2,20 +2,26 @@ define(function (require) {
     'use strict';
 
     var slider = require('../plugin/slider');
+    var result = document.querySelector('#result');
 
     var sliderExample = slider(document.querySelector('#slider-example'));
     sliderExample.on('slidestart', function (e) {
         if (Math.random() > 0.5) {
             e.preventDefault();
             console.log(e.type, 'prevented');
+            result.innerHTML = e.type + ', ' + 'prevented';
         } else {
             console.log(e.type);
+            result.innerHTML = e.type;
         }
     }).on('slidestarted', function (e) {
         console.log(e.type);
+        result.innerHTML = e.type;
     }).on('slidemoved', function (e) {
         console.log(e.type);
+        result.innerHTML = e.type;
     }).on('slideended', function (e) {
         console.log(e.type);
+        result.innerHTML = e.type;
     });
 });
